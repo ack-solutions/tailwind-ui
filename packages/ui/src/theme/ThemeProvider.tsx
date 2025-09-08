91d31498c15c1ebdd5fad53e0024d2a593bf6344
Chetan Khandla <ckhandla94@gmail.com>
2025-09-08 10:03:47 +0530

    Revert "chore: initialize ackplus-ui monorepo"

--- packages/ui/src/theme/ThemeProvider.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface ThemeProviderProps {
-  children: React.ReactNode;
-}
-
-export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => (
-  <>{children}</>
-);
